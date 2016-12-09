5e25e06161aa400cdcd2ad9bf95adbf0545fa47c
James Howard <james@>
2016-12-09 09:34:33 -0800

    Add StylistTest.cs to tests forlder and populated it. Add Stylist.cs to object folder and created a constructor and getters and setters.

---
+++ Objects/Stylist.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  public class Stylist
+  {
+    private int _id;
+    private string _name;
+
+    public Stylist(string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+
+    public void SetId(int Id)
+    {
+      _id = Id;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public void SetName(string Name)
+    {
+      _name = Name;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+  }
+}
---
+++ Tests/StylistTest.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Date;
+using System.Data.SqlClient;
+using Xunit;
+
+namespace HairSalon
+{
+  public class stylistTest : IDisposable
+  {
+    public stylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(86QO71)\\James(54); Initial Catalog=hair_salon_test; Integrated Security=SSPI;";
+    }
+    public void Dispose()
+    {
+      Stylist.DeleteAll();
+    }
+  }
+}
