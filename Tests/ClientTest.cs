743ed9159c6b8ce6dea38da31fd6284240f5593c
James Howard <james@>
2016-12-09 11:49:56 -0800

    Add Client.cs ClientTest.cs and built contructor and Getters and setters.

---
+++ Tests/ClientTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Xunit;
+
+namespace HairSalon
+{
+  public class ClientTest : IDisposable
+  {
+    public ClientTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=DESKTOP-86RQO71; Initial Catalog=hair_salon_test; Integrated Security=SSPI;";
+    }
+
+    public void Dispose()
+    {
+      Client.DeleteAll();
+    }
+
+  }
+}
