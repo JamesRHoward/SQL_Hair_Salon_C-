f0129b2e3d54babcfafcf58fe31b08772696251e
James Howard <james@>
2016-12-09 09:51:08 -0800

    Add GetAll method to Stylist.cs

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -45,5 +45,30 @@ namespace HairSalon
       cmd.ExecuteNonQuery();
       conn.Close();
     }
+
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string stylistName = rdr.GetString(1);
+        Stylist newStylist = new Stylist(stylistName, stylistId);
+      }
+      if(rdr!=null)
+      {
+        rdr.Close();
+      }
+      if(conn!=null)
+      {
+        conn.Close();
+      }
+    }
   }
 }
